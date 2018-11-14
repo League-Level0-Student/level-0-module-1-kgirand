c8a885e4a1f4e189f509b4d7443b5e0b9c2cb808
11002084 <League@AdministorsiMac.attlocal.net>
2018-11-13 19:51:29 -0800

    Level 0 Module 2 I think

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,4 +1,7 @@
 package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -9,11 +12,17 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+String Answer1 = "A coffin";
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String Input1 = JOptionPane.showInputDialog("Who makes it, has no need of it. Who buys it, has no use for it. Who uses it can neither see nor feel it.  What is it?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(Input1.equals(Answer1)) {
+	JOptionPane.showInternalMessageDialog(null, "Correct! +1 point!");
+	score ++;
+	
+	
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
